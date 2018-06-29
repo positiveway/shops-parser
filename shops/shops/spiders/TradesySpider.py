d548fc4a2a99274764facb0a1873a24856828b26
Andrew <coreand.work@gmail.com>
2018-06-29 11:17:40 +0400

    Parsing of base parameters

--- shops/shops/spiders/TradesySpider.py
+++ shops/shops/spiders/TradesySpider.py
@@ -11,7 +11,34 @@ class TradesySpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        pass
+        for bag in response.css('a.item-image[href^="/i/"]::attr(href)').extract():
+            yield response.follow(bag, callback=self.parse_bag)
+
+    def parse_bag(self, response):
+        brand = response.css('#idp-brand > a.tags::text').extract_first()
+
+        model = response.css('#idp-title::text').extract_first()
+        model = model.strip()
+
+        size = response.css('#idp-size-id > span.item-size::text').extract_first()
+        size = size.strip()
+
+        price = response.css('#idp-price > div.idp-price-wrapper > div > div.item-price::text').extract_first()
+        price = re.search('[.,\d]+', price).group(0)
+        price = price.replace(',', '')
+        price = float(price)
+
+        condition = response.css(
+            '#idp-info > div:nth-child(1) > div.idp-condition.idp-info-accordion > div > b::text').extract_first()
+
+        yield {
+            'brand': brand,
+            'model': model,
+            'size': size,
+            'price': price,
+            'condition': condition,
+            'url': response.url,
+        }
 
 
 if __name__ == '__main__':
